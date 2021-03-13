bb3baa0bc816f07780b633623a8a492534a9579a
Grusker <37528419+grusker@users.noreply.github.com>
2021-03-13 11:00:12 +0300

    First Commit

---
+++ public-message-board/src/main/java/com/grusker/publicmessageboard/PublicMessageBoardApplication.java
@@ -0,0 +1,13 @@
+package com.grusker.publicmessageboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicMessageBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicMessageBoardApplication.class, args);
+	}
+
+}
---
+++ public-message-board/src/test/java/com/grusker/publicmessageboard/PublicMessageBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grusker.publicmessageboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicMessageBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
