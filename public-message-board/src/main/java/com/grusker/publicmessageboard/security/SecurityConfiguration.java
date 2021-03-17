3ba27615216781929f0c2951f50101e53d9b9393
grusker <37528419+grusker@users.noreply.github.com>
2021-03-17 22:05:46 +0300

    Session was made stateless to prevent cached authentication

--- public-message-board/src/main/java/com/grusker/publicmessageboard/security/SecurityConfiguration.java
+++ public-message-board/src/main/java/com/grusker/publicmessageboard/security/SecurityConfiguration.java
@@ -1,10 +1,12 @@
 package com.grusker.publicmessageboard.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -13,8 +15,12 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
-       httpSecurity.csrf().disable()
+       httpSecurity
+               .sessionManagement()
+               .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+               .and()
                .authorizeRequests()
+               .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                .anyRequest().authenticated()
                .and()
                .httpBasic();
